628f17c580d321a6363dbde3e87e3853cccfd7d2
bnsant <bernardosanlima@gmail.com>
2025-11-04 17:39:08 -0300

    feat(backend): importa modelos, DAOs e classe Conexao do projeto anterior

---
+++ Sistemas_Distribuidos_Backend_A3/src/main/java/com/mycompany/sistemas_distribuidos_backend_a3/Sistemas_Distribuidos_Backend_A3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemas_distribuidos_backend_a3;
+
+/**
+ *
+ * @author berna
+ */
+public class Sistemas_Distribuidos_Backend_A3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
